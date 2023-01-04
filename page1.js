d097157a821a9db3d8fd8c04201e1c2958da45bf
Evan-Tan09 <84255090+Evan-Tan09@users.noreply.github.com>
2023-01-04 21:11:27 +0800

    Add files via upload

---
+++ page1.js
@@ -0,0 +1,38 @@
+img = "";
+status = "";
+results = [];
+function setup() {
+    canvas = createCanvas(800, 400);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting objects";
+}
+function modelLoaded() {
+    console.log("Model loaded");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error, r) {
+    if (error) {
+        console.log("error");
+    } else {
+        results = r;
+        console.log(results);
+    }
+}
+function preload() {
+    img = loadImage("ball.JPG");
+}
+function draw() {
+    for (i = 0; i < results.length; i++) {
+        image(img, 0, 0, 800, 400);
+        noFill();
+        stroke("#ff00ff");
+        a = results[0].normalized.x * 800;
+        b = results[0].normalized.y * 400;
+        c = results[0].normalized.width * 800;
+        d = results[0].normalized.height * 400;
+        rect(a, b, c, d);
+    }
+    //text(detectedLabel, detectedX / 10, detectedY / 10 - 10);
+}
